8952f2c2fc51a98c42f6ddef68ea70e76532058e
MugzLord <michael_moon007@yahoo.com>
2025-12-17 23:28:37 +0300

    Update santa_bot.py

--- santa_bot.py
+++ santa_bot.py
@@ -331,7 +331,7 @@ class SantaWishModal(discord.ui.Modal, title="Send a Wish to Santa"):
         label="Recipient (optional) — @mention or ID",
         required=False,
         max_length=80,
-        placeholder="@Eli or 123456789012345678"
+        placeholder="Discord ID or Username "
     )
 
     anon_message = discord.ui.TextInput(
